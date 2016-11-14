e824bccc50ce8576e4ae8707e486f65e0ac80d9e
Stéphane <Stephanyan@users.noreply.github.com>
2016-11-14 16:46:29 +0100

    Update ChristmasTree.java
    int j = 1 instead of 0 because I want it sticking to the left margin

--- ChristmasTree.java
+++ ChristmasTree.java
@@ -8,7 +8,7 @@ public class ChristmasTree {
 
         for (int i = 0; i < number; i++){
 
-            for (int j = 0; j < number-i; j++){
+            for (int j = 1; j < number-i; j++){
                 System.out.print(" ");
             }
             for (int k = 0; k < (2*(i)+1); k++) {
